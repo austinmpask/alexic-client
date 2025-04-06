a6eccad44b988751be491249fd8b4361622b4c62
mrbeans100 <135092377+austinmpask@users.noreply.github.com>
2025-04-06 06:51:39 -0700

    Update games.js

--- src/games.js
+++ src/games.js
@@ -2,7 +2,7 @@ export const games = {
   "04032025": ["STAR", "MOON"],
   "04042025": ["CAVE", "HILL"],
   "04052025": ["HAND", "PALM", "BAND", "XAND"],
-  "04062025": ["SHIP", "DOCK", "LOCK", "UOCK"],
+  "04062025": ["SHIP", "DOCK", "CHIP", "XHIP"],
   "04072025": ["TREE", "LEAF"],
   "04082025": ["WIND", "BLOW"],
 };
