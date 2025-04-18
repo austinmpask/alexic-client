2a7201b5349dd27d0043df675214866cb8399323
mrbeans100 <austinmpask@gmail.com>
2025-04-18 13:47:21 -0700

    change modal

--- src/components/ModalFrame.jsx
+++ src/components/ModalFrame.jsx
@@ -1,6 +1,6 @@
 import Loader from "./Loader";
 
-import ReactModal from "react-modal";
+// import ReactModal from "react-modal";
 import Logo from "./Logo";
 import { useContext } from "react";
 import { GameStateContext } from "../GameState";
@@ -13,35 +13,36 @@ export default function ModalFrame({
 }) {
   const { gameState, setGameState } = useContext(GameStateContext);
   return (
-    <ReactModal
-      isOpen={gameState.isModalOpen}
-      className={`welcome-modal ${
-        gameState.isLoading ? "justify-center" : "justify-between"
-      }`}
-    >
-      {gameState.isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <Logo />
-          {children}
-          <button
-            disabled={gameState.stage >= 2}
-            onClick={() => {
-              setGameState((p) => ({ ...p, isModalOpen: false }));
-              gameState.stage === 0 &&
-                setGameState((p) => ({ ...p, stage: 1 }));
-            }}
-            className={`${
-              gameState.stage >= 2
-                ? "border-neutral-200 bg-neutral-300"
-                : "bg-purple-400 border-2 border-purple-300"
-            }  py-3 cursor-pointer px-6 text-xl rounded-2xl text-white`}
-          >
-            {gameState.stage === 0 ? "Play" : "Back"}
-          </button>
-        </>
-      )}
-    </ReactModal>
+    gameState.isModalOpen && (
+      <div
+        className={`welcome-modal ${
+          gameState.isLoading ? "justify-center" : "justify-between"
+        }`}
+      >
+        {gameState.isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <Logo />
+            {children}
+            <button
+              disabled={gameState.stage >= 2}
+              onClick={() => {
+                setGameState((p) => ({ ...p, isModalOpen: false }));
+                gameState.stage === 0 &&
+                  setGameState((p) => ({ ...p, stage: 1 }));
+              }}
+              className={`${
+                gameState.stage >= 2
+                  ? "border-neutral-200 bg-neutral-300"
+                  : "bg-purple-400 border-2 border-purple-300"
+              }  py-3 cursor-pointer px-6 text-xl rounded-2xl text-white`}
+            >
+              {gameState.stage === 0 ? "Play" : "Back"}
+            </button>
+          </>
+        )}
+      </div>
+    )
   );
 }
