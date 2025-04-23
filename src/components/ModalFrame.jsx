7b225a6346d49593643e1fb98d68d1ad02dfd538
mrbeans100 <austinmpask@gmail.com>
2025-04-23 09:41:51 -0700

    add animation to modal

--- src/components/ModalFrame.jsx
+++ src/components/ModalFrame.jsx
@@ -1,9 +1,11 @@
 import Loader from "./Loader";
+import { AnimatePresence, motion } from "motion/react";
 
 // import ReactModal from "react-modal";
 import Logo from "./Logo";
 import { useContext, useEffect } from "react";
 import { GameStateContext } from "../GameState";
+import { modalTransition } from "../config";
 
 // Modal which displays either a loading thing or some child content
 export default function ModalFrame({
@@ -34,36 +36,42 @@ export default function ModalFrame({
   //   return () => window.removeEventListener("resize", adjustModalHeight);
   // }, [gameState.isModalOpen]);
   return (
-    gameState.isModalOpen && (
-      <div
-        className={`welcome-modal ${
-          gameState.isLoading ? "justify-center" : "justify-between"
-        }`}
-      >
-        {gameState.isLoading ? (
-          <Loader />
-        ) : (
-          <>
-            <Logo />
-            {children}
-            <button
-              disabled={gameState.stage >= 2}
-              onClick={() => {
-                setGameState((p) => ({ ...p, isModalOpen: false }));
-                gameState.stage === 0 &&
-                  setGameState((p) => ({ ...p, stage: 1 }));
-              }}
-              className={`${
-                gameState.stage >= 2
-                  ? "border-neutral-200 bg-neutral-300"
-                  : "bg-purple-400 border-2 border-purple-300"
-              }  py-3 cursor-pointer px-6 text-xl rounded-2xl text-white`}
-            >
-              {gameState.stage === 0 ? "Play" : "Back"}
-            </button>
-          </>
-        )}
-      </div>
-    )
+    <AnimatePresence>
+      {gameState.isModalOpen && (
+        <motion.div
+          initial={{ translateY: 30, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          exit={{ translateY: 30, opacity: 0 }}
+          transition={modalTransition}
+          className={`welcome-modal ${
+            gameState.isLoading ? "justify-center" : "justify-between"
+          }`}
+        >
+          {gameState.isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              <Logo />
+              {children}
+              <button
+                disabled={gameState.stage >= 2}
+                onClick={() => {
+                  setGameState((p) => ({ ...p, isModalOpen: false }));
+                  gameState.stage === 0 &&
+                    setGameState((p) => ({ ...p, stage: 1 }));
+                }}
+                className={`${
+                  gameState.stage >= 2
+                    ? "border-neutral-200 bg-neutral-300"
+                    : "bg-purple-400 border-2 border-purple-300"
+                }  py-3 cursor-pointer px-6 text-xl rounded-2xl text-white`}
+              >
+                {gameState.stage === 0 ? "Play" : "Back"}
+              </button>
+            </>
+          )}
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
--- src/config.js
+++ src/config.js
@@ -2,3 +2,4 @@ export const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 export const url = "https://alexic.io/";
 export const api = "https://api.alexic.io/";
 export const emojis = ["⬛", "🟪"];
+export const modalTransition = { duration: 0.3, ease: [0.28, 0.17, 0.05, 1.0] };
