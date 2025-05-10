8e8b00b8e18eb964afa1b2962bbb91d5c7d5e952
austin <austinmpask@gmail.com>
2025-05-10 12:26:14 -0700

    [ADD] wiggle golden word on game start

--- src/components/GameHeader.jsx
+++ src/components/GameHeader.jsx
@@ -1,12 +1,64 @@
-import { useContext } from "react";
-import { GameStateContext } from "../GameState";
-import { toCombo, toWord } from "../utils";
 import { CircleHelp, Pyramid, Undo } from "lucide-react";
+import { animate } from "motion";
+import { useContext, useEffect, useRef } from "react";
+import { GameStateContext } from "../GameState";
 import { UIContext } from "../UIState";
+import { toCombo, toWord } from "../utils";
 
-export default function GameHeader({}) {
+export default function GameHeader() {
   const game = useContext(GameStateContext);
   const { openModal, touch } = useContext(UIContext);
+
+  const goldenRef = useRef();
+
+  const animationAmounts = {
+    scale: 1.25,
+    rotate: 5,
+  };
+
+  useEffect(() => {
+    game.stage === 1 &&
+      goldenRef.current &&
+      setTimeout(() => {
+        // console.log("boom");
+        const sequence = [
+          // Scale up
+          [
+            goldenRef.current,
+            { scale: animationAmounts.scale },
+            { duration: 0.3 },
+          ],
+          // Pause
+          [
+            goldenRef.current,
+            { scale: animationAmounts.scale },
+            { duration: 0.05 },
+          ],
+          // Wiggle
+          [
+            goldenRef.current,
+            { rotate: animationAmounts.rotate },
+            { duration: 0.1 },
+          ],
+          [
+            goldenRef.current,
+            { rotate: -animationAmounts.rotate },
+            { duration: 0.1 },
+          ],
+          [
+            goldenRef.current,
+            { rotate: animationAmounts.rotate },
+            { duration: 0.1 },
+          ],
+          [goldenRef.current, { rotate: 0 }, { duration: 0.1 }],
+          // Pause
+          [goldenRef.current, { rotate: 0 }, { duration: 0.1 }],
+          // Scale down
+          [goldenRef.current, { scale: 1.0 }, { duration: 0.3 }],
+        ];
+        animate(sequence);
+      }, 1100);
+  }, [game.stage, goldenRef.current]);
 
   function handleUndo() {
     const combo = toCombo(game.getLastWord());
@@ -34,7 +86,10 @@ export default function GameHeader({}) {
         >
           <Undo />
         </button>
-        <div className="select-none border-2 box-glow border-amber-200 bg-amber-100 rounded-3xl text-lg sm:text-2xl font-semibold tracking-widest flex flex-row items-center justify-center gap-2 text-amber-500 py-3 px-4">
+        <div
+          ref={goldenRef}
+          className="select-none border-2 box-glow border-amber-200 bg-amber-100 rounded-3xl text-lg sm:text-2xl font-semibold tracking-widest flex flex-row items-center justify-center gap-2 text-amber-500 py-3 px-4"
+        >
           <Pyramid className="size-4.5 sm:size-6" />
           {game.goldenWord}
         </div>
